4251cd7222f232a4ac78712a4ff45ca602feed7b
Romanzes <petrenko_r@mail.ru>
2013-10-22 18:55:13 +0400

    Add javadoc

--- LibNot/src/ru/footmade/libnot/NegationUtils.java
+++ LibNot/src/ru/footmade/libnot/NegationUtils.java
@@ -5,6 +5,11 @@ public class NegationUtils {
     private static final Boolean TRUE = Boolean.valueOf(true);
     private static final Boolean FALSE = Boolean.valueOf(false);
 
+    /**
+     * Does logical negation
+     * @param var a value which should be negated. Must be either true or false.
+     * @return true, if an argument is false; false, if an argument is true.
+     */
     public static boolean not(boolean var) {
         Boolean varObj = Boolean.valueOf(var);
         if (varObj.equals(TRUE)) {
