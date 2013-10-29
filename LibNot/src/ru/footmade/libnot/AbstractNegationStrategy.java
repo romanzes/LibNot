fcf8fadeae149f9df3072dc435c36fcbebdfc067
Romanzes <petrenko_r@mail.ru>
2013-10-29 18:53:24 +0400

    Implement asynchronous method calls

--- LibNot/src/ru/footmade/libnot/AbstractNegationStrategy.java
+++ LibNot/src/ru/footmade/libnot/AbstractNegationStrategy.java
@@ -5,6 +5,31 @@ public abstract class AbstractNegationStrategy {
      * Does logical negation
      * @param var a value which should be negated. Must be either true or false.
      * @return true, if an argument is false; false, if an argument is true.
+     * @see #notAsync(boolean, ru.footmade.libnot.AbstractNegationStrategy.AsyncCallback)
      */
     public abstract boolean not(boolean var);
+    
+    /**
+     * Does asynchronous logical negation
+     * @param var a value which should be negated. Must be either true or false.
+     * @param callback true, if an argument is false; false, if an argument is true.
+     * @see #not(boolean)
+     */
+    public void notAsync(final boolean var, final AsyncCallback callback) {
+        (new Thread() {
+            @Override
+            public void run() {
+                boolean result = not(var);
+                if (callback != null)
+                    callback.processResult(result);
+            }
+        }).start();
+    }
+    
+    /**
+     * Callback interface to process the result of an asynchronous negation
+     */
+    public static interface AsyncCallback {
+        public void processResult(boolean result);
+    }
 }
--- LibNot/test/ru/footmade/libnot/NegationTest.java
+++ LibNot/test/ru/footmade/libnot/NegationTest.java
@@ -25,33 +25,89 @@ public class NegationTest {
     }
     
     @Test
-    public void test() {
+    public void strictTest() {
+        testCase(defaultStrategy, true, false, true);
+        testCase(defaultStrategy, true, false, false);
+        testCase(defaultStrategy, false, true, true);
+        testCase(defaultStrategy, false, true, false);
+        testCase(cachedStrategy, true, false, true);
+        testCase(cachedStrategy, true, false, false);
+        testCase(cachedStrategy, false, true, true);
+        testCase(cachedStrategy, false, true, false);
+    }
+    
+    /**
+     * In case we have overlooked something
+     */
+    @Test
+    public void randomTest() {
         for (int i = 0; i < 1000; i++) {
             testIteration();
         }
     }
     
+    private class FlagHolder {
+        private boolean _flag;
+        
+        public void mark() {
+            _flag = true;
+        }
+        
+        public boolean marked() {
+            return _flag;
+        }
+    }
+    
+    private void testCase(AbstractNegationStrategy strategy, boolean value,
+            final boolean expect, boolean async) {
+        final String failMessage = String.format("Not %s must be %s", value, expect);
+        if (async) {
+            final FlagHolder asyncFlag = new FlagHolder();
+            strategy.notAsync(value, new AbstractNegationStrategy.AsyncCallback() {
+                @Override
+                public void processResult(boolean result) {
+                    assertEquals(failMessage, expect, result);
+                    asyncFlag.mark();
+                }
+            });
+            while (!asyncFlag.marked()) {
+                try { Thread.sleep(0); } catch (InterruptedException ex) {}
+            }
+        } else {
+            assertEquals(failMessage, expect, strategy.not(value));
+        }
+    }
+    
     public void testIteration() {
         AbstractNegationStrategy strategy;
+        boolean value;
+        boolean expect;
+        boolean async;
         
         switch (rand.nextInt(2)) {
             case 0:
                 strategy = defaultStrategy;
+                value = true;
+                expect = false;
                 break;
             case 1:
             default:
                 strategy = cachedStrategy;
+                value = false;
+                expect = true;
                 break;
         }
         
         switch (rand.nextInt(2)) {
             case 0:
-                assertEquals("Not true must be false", false, strategy.not(true));
+                async = false;
                 break;
             case 1:
             default:
-                assertEquals("Not false must be true", true, strategy.not(false));
+                async = true;
                 break;
         }
+        
+        testCase(strategy, value, expect, async);
     }
 }
